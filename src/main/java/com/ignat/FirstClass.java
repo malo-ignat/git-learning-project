d7ae3890d5b88d2943e86d234f0011cc1451a437
malo65 <malo65@yandex.com>
2022-05-26 21:46:14 +0300

    Change FirstClass

--- src/main/java/com/ignat/FirstClass.java
+++ src/main/java/com/ignat/FirstClass.java
@@ -3,5 +3,6 @@ package com.ignat;
 public class FirstClass {
     public static void main(String[] args) {
         System.out.println("Hello from first");
+        System.out.println("Line from second commit");
     }
 }
